3d05178a90c616e14e58962ef53707370581d9e6
Emilia <emilia.skrzypczak.es@gmail.com>
2022-09-20 18:13:42 +0200

    Add functions to make the calculator work

--- javascript.js
+++ javascript.js
@@ -1,11 +1,11 @@
- //add (addEventListener to the "+" button, on "click" run this function:)
+ //add 
  function add(...args) {
     let total = 0;
     for (const arg of args) {
       total += arg;
     }
     return total;
-  }
+  };
   //substract
   function substract(...args) {
     let total = 0;
@@ -17,7 +17,7 @@
       }
     }
     return total;
-  }
+  };
   //multiply
   function multiply(...args) {
     let total = 0;
@@ -59,5 +59,180 @@
     } else if (operator === '/') {
       return divide(num1,num2);
     }
-  }
-  console.log();
+  };
+
+  let display = document.querySelector(".display");
+  //clear display:
+  let clearbtn = document.querySelector(".clear");
+  clearbtn.addEventListener("click", () => {
+    display.textContent = 0;
+  });
+
+  //click and display numbers:
+  let numberbtns = document.querySelectorAll(".number");
+  Array.from(numberbtns).forEach(numberbtn => numberbtn.addEventListener("click", () =>{
+    if (display.textContent === "0" || 0) {
+        display.textContent = "";
+    }
+    if (display.textContent.length === 11) {
+        return;
+    }
+    display.textContent += numberbtn.textContent;
+  }));
+    Array.from(numberbtns).forEach(numberbtn => numberbtn.addEventListener("mousedown", () => {
+        if (display.textContent.length === 11) {
+                numberbtn.classList.add("wrong");
+                return;
+        }
+    }));
+    Array.from(numberbtns).forEach(numberbtn => numberbtn.addEventListener("mouseover", () => {
+        if (display.textContent.length === 11) {
+                numberbtn.classList.add("wrong");
+                return;
+        }
+        numberbtn.classList.remove("wrong");
+    }));
+
+  //backspace display:
+  let backspacebtn = document.querySelector(".backspace");
+  backspacebtn.addEventListener("click", () => {
+    display.textContent = display.textContent.slice(0,-1);
+    if (display.textContent === "") {
+        display.textContent = 0;
+    }
+  });
+
+  function check() {
+    if (display.textContent.includes("+") ||
+    display.textContent.slice(1).includes("-") ||
+    display.textContent.includes("*") ||
+    display.textContent.includes("/")){
+        return true;
+    }
+  };
+
+  //add decimal to the display:
+  let decimalbtn = document.querySelector(".decimal");
+  decimalbtn.addEventListener("click", () => {
+    if (display.textContent.includes(".") || display.textContent.length === 11) {
+        return;
+    }
+    display.textContent += decimalbtn.textContent;
+  });
+  decimalbtn.addEventListener("mouseover", () => {
+    if (display.textContent.includes(".") || display.textContent.length === 11) {
+            decimalbtn.classList.add("wrong");
+            return;
+    }
+    decimalbtn.classList.remove("wrong");
+  });
+
+  //add operation:
+  let plusbtn = document.querySelector(".plus");
+  plusbtn.addEventListener("click", () => {
+    if (check() || display.textContent.length === 11) {
+        return;
+    }
+    display.textContent += plusbtn.textContent;
+  });
+  plusbtn.addEventListener("mouseover", () => {
+    if (check() || display.textContent.length === 11) {
+            plusbtn.classList.add("wrong");
+            return;
+    }
+    plusbtn.classList.remove("wrong");
+  });
+
+  //substract operation:
+  let minusbtn = document.querySelector(".minus");
+  minusbtn.addEventListener("click", () => {
+    if (check() || display.textContent.length === 11) {
+            return;
+    }
+    if (display.textContent === "0" || 0) {
+        display.textContent = "";
+    }
+    display.textContent += minusbtn.textContent;
+  });
+  minusbtn.addEventListener("mouseover", () => {
+    if (check() || display.textContent.length === 11) {
+            minusbtn.classList.add("wrong");
+            return;
+    }
+    minusbtn.classList.remove("wrong");
+  });
+
+  //multiply operation:
+  let multiplybtn = document.querySelector(".multiply");
+  multiplybtn.addEventListener("click", () => {
+    if (check() || display.textContent.length === 11) {
+            return;
+    }
+    display.textContent += '*';
+  });
+  multiplybtn.addEventListener("mouseover", () => {
+    if (check() || display.textContent.length === 11) {
+            multiplybtn.classList.add("wrong");
+            return;
+    }
+    multiplybtn.classList.remove("wrong");
+  });
+
+  //divide operation:
+  let dividebtn = document.querySelector(".divide");
+  dividebtn.addEventListener("click", () => {
+    if (check() || display.textContent.length === 11) {
+            return;
+    }
+    display.textContent += '/';
+  });
+  dividebtn.addEventListener("mouseover", () => {
+    if (check() || display.textContent.length === 11) {
+            dividebtn.classList.add("wrong");
+            return;
+    }
+    dividebtn.classList.remove("wrong");
+  });
+
+  //equals operation:
+let equalsbtn = document.querySelector(".equals");
+  equalsbtn.addEventListener("click", () => {
+    if (display.textContent.at(-1) === ("+") ||
+    display.textContent.at(-1) === ("-") ||
+    display.textContent.at(-1) === ("*") ||
+    display.textContent.at(-1) === ("/") ||
+    display.textContent.at(-1) === (".")) {
+        return;
+    }
+    let num1and2;
+    let myOperator;
+    if (display.textContent.includes("+")) {
+        myOperator = "+";
+        num1and2 = display.textContent.split("+");
+
+    } else if (display.textContent.charAt(0) === "-" && display.textContent.slice(1).includes("-")) {
+        myOperator = "-";
+        num1and2 = display.textContent.split("-");
+        num1and2[0] = `-${num1and2[1]}`;
+        num1and2[1] = num1and2[2];
+    } else if (display.textContent.slice(1).includes("-") && !(display.textContent.charAt(0) === "-")) {
+        myOperator = "-";
+        num1and2 = display.textContent.split("-");
+
+    } else if (display.textContent.includes("*")) {
+        myOperator = "*";
+        num1and2 = display.textContent.split("*");
+    } else if (display.textContent.includes("/")) {
+        myOperator = "/";
+        num1and2 = display.textContent.split("/");
+        if (num1and2[1] === '0') {
+            alert("Oops! You tried to divide by 0! Don't worry, I'm a computer, I won't tell your maths teacher! :)");
+            return;
+        }
+    }
+    let number1 = Number(num1and2[0]);
+    let number2 = Number(num1and2[1]);
+    let result = operate(myOperator, number1, number2);
+    display.textContent = result;
+    display.textContent = Math.round(Number(display.textContent) * 100) / 100;
+  });
